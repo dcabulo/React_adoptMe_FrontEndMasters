d0b82ffa3411132b9541012d2573f45344e31f90
Dcab <gcabulo@gmail.com>
2020-11-02 19:01:55 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import { Link, Router } from "@reach/router";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import themeContext from "./ThemeContext";
+
+const App = () => {
+  const themeHook = useState("peru");
+  return (
+    <themeContext.Provider value={themeHook}>
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </themeContext.Provider>
+  );
+};
+
+render(<App />, document.getElementById("root"));
